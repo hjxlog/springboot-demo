30edc4f5574d96d8d02d785ae7b527d83d0efbe2
HuangJX <hjxlog@gmail.com>
2023-02-27 08:21:12 +0800

    update

---
+++ mybatisplus/src/main/java/com/hjxlog/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.hjxlog.mapper;
+
+import com.hjxlog.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author hjx
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2023-02-26 23:31:17
+* @Entity com.hjxlog.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ mybatisplus/src/test/java/com/hjxlog/SampleTest.java
@@ -0,0 +1,24 @@
+package com.hjxlog;
+
+import com.hjxlog.domain.User;
+import com.hjxlog.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
+    }
+
+}
